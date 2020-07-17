5f9072aca32eebb12a49ce2e5b561f0dd2129220
Lauren Stone <lstone321@gmail.com>
2020-07-17 02:09:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,16 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split
   reverse_array = new_sentence.map do |word|
-  word.reverse
+ word.reverse
 end
     "#{word}".reverse
  end
 end
 
+def reverse_each_word(sentence)
+  new_sentence= sentence.split
+  reverse_array = new_sentence.map do |word|
+  word.reverse
+end
+
+  end
