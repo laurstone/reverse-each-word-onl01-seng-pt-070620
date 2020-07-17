8de015aed85da31f63c8c7acd00c769f27b6e2d3
Lauren Stone <lstone321@gmail.com>
2020-07-17 02:03:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,12 @@ def reverse_each_word(sentence)
     "#{word}".reverse
  end
 end
+
+def reverse_each_word(sentence)
+  new_sentence = sentence.split
+  new_sentence.map do |word|
+    "#{word}".reverse
+    
+  end
+  
+  end
